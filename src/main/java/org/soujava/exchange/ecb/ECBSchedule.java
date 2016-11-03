4e20d2b52a4651837c3eac7599f3707b199f6fa5
Otavio Santana <otaviojava@java.net>
2016-11-03 19:51:58 -0200

    Adds dependency to tests

---
+++ src/main/java/org/soujava/exchange/ecb/ECBSchedule.java
@@ -0,0 +1,7 @@
+package org.soujava.exchange.ecb;
+
+/**
+ * Created by otaviojava on 11/1/16.
+ */
+public class ECBSchedule {
+}
---
+++ src/main/java/org/soujava/exchange/ecb/ECBScheduleTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by otaviojava on 11/3/16.
+ */
+public class ECBScheduleTest {
+
+}
